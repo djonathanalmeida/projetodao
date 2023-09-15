5176027ca0b53a713fc71fe5b7710ebde426f460
Djonathan Almeida <djonathan_7@hotmail.com>
2023-09-15 13:44:04 -0300

    Classe entidade vendedor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,19 @@
 package aplicacao;
 
+import java.util.Date;
+
 import model.entidades.Departamento;
+import model.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1, "Livros");
-		System.out.println(obj);
+		
+		Vendedor vend = new Vendedor(21, "Bob", "bob@email.com", new Date (), 3000.0, obj);
+		
+		System.out.println(vend);
 
 	}
 
