feade2a8cdec58077dc52c15ebf2d802c67cc53b
Djonathan Almeida <djonathan_7@hotmail.com>
2023-09-25 13:09:59 -0300

    Implementacao update

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -13,28 +13,34 @@ public class Programa {
 	public static void main(String[] args) {
 
 		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
-		
+
 		System.out.println("===== TESTE 1: vendedor findById =====");
 		Vendedor vendedor = vendedorDao.findById(3);
 		System.out.println(vendedor);
-		
+
 		System.out.println("===== TESTE 2: vendedor findByDepartment =====");
 		Departamento departamento = new Departamento(2, null);
 		List<Vendedor> list = vendedorDao.findByDepartament(departamento);
-		for(Vendedor obj : list) {
+		for (Vendedor obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("===== TESTE 3: vendedor findAll =====");
 		list = vendedorDao.findAll();
-		for(Vendedor obj : list) {
+		for (Vendedor obj : list) {
 			System.out.println(obj);
 		}
-		
+
 		System.out.println("===== TESTE 4: vendedor insert =====");
 		Vendedor novoVendedor = new Vendedor(null, "Greg", "greg@email.com", new Date(), 4000.0, departamento);
 		vendedorDao.insert(novoVendedor);
 		System.out.println("Inserido! Novo id= " + novoVendedor.getId());
+
+		System.out.println("===== TESTE 5: vendedor update =====");
+		vendedor = vendedorDao.findById(1);
+		vendedor.setNome("Martha Waine");
+		vendedorDao.update(vendedor);
+		System.out.println("Update completado!");
 
 	}
 
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -54,8 +54,7 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
 			DB.closeStatement(st);
 		}
 
@@ -63,6 +62,26 @@ public class VendedorDaoJDBC implements VendedorDao {
 
 	@Override
 	public void update(Vendedor obj) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " + "WHERE Id = ?");
+
+			st.setString(1, obj.getNome());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getDataNascimento().getTime()));
+			st.setDouble(4, obj.getSalarioBase());
+			st.setInt(5, obj.getDepartamento().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+		}
+
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
